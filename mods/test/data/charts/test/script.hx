d04b52edeb9313254406c274df0853603044539e
Gidk-g <devhaxel@gmail.com>
2025-05-04 23:57:42 +0200

    Update script.hx

--- mods/test/data/charts/test/script.hx
+++ mods/test/data/charts/test/script.hx
@@ -2,6 +2,48 @@ function createPost() {
     this.camHUD.alpha = 0;
 }
 
+function noteMiss(note) {
+    this.camHUD.shake(0.009,0.2);
+}
+
+function goodNoteHit(note) {
+    if (!note.isSustainNote) {
+        if (FlxG.save.data.downscroll) {
+            this.playerStrums.members[Math.round(Math.abs(note.noteData))].y += 20;
+        } else {
+            this.playerStrums.members[Math.round(Math.abs(note.noteData))].y -= 20;  
+        }
+    }
+}
+
+function dadNoteHit(daNote) {
+    if (!daNote.isSustainNote) {
+        if (FlxG.save.data.downscroll) {
+            this.dadStrums.members[Math.round(Math.abs(daNote.noteData))].y += 20;
+        } else {
+            this.dadStrums.members[Math.round(Math.abs(daNote.noteData))].y -= 20;  
+        }
+    }
+}
+
+function update(elapsed) {
+    for (i in 0...this.playerStrums.members.length) {
+        if (Config.downScroll) {
+            this.playerStrums.members[i].y = FlxMath.lerp(FlxG.height - 160, this.playerStrums.members[i].y, FlxMath.bound(1 - (elapsed * 6), 0 , 1));
+        } else {
+            this.playerStrums.members[i].y = FlxMath.lerp(70, this.playerStrums.members[i].y, FlxMath.bound(1 - (elapsed * 6), 0 , 1));
+        }
+    }
+
+    for (i in 0...this.dadStrums.members.length) {
+        if (Config.downScroll) {
+            this.dadStrums.members[i].y = FlxMath.lerp(FlxG.height - 160, this.dadStrums.members[i].y, FlxMath.bound(1 - (elapsed * 6), 0 , 1));
+        } else {
+            this.dadStrums.members[i].y = FlxMath.lerp(70, this.dadStrums.members[i].y, FlxMath.bound(1 - (elapsed * 6), 0 , 1));
+        }
+    }
+}
+
 function stepHit(curStep) {
     switch (curStep){
         case 632:
@@ -21,6 +63,20 @@ function beatHit(curBeat) {
     if (Config.camZooms && curBeat > 96 && curBeat <= 112){
         FlxG.camera.zoom += 0.020;
         this.camHUD.zoom += 0.01;
+    }
+
+    if (curBeat % 4 == 0 && curBeat >= 64 && curBeat <= 128) {
+        FlxTween.tween(this.camHUD, {zoom: 1.05}, 0.12, {
+            ease: FlxEase.sineInOut,
+            onComplete: function(_) {
+                FlxTween.tween(this.camHUD, {zoom: 0.98}, 0.12, {
+                    ease: FlxEase.sineInOut,
+                    onComplete: function(_) {
+                        FlxTween.tween(this.camHUD, {zoom: 1}, 0.08, {ease: FlxEase.sineInOut});
+                    }
+                });
+            }
+        });
     }
 
     switch (curBeat){
